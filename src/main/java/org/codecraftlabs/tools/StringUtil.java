544c366820360c78da22172cf1259782436fd98e
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2021-06-12 21:38:41 +0200

    Initial commit

---
+++ src/main/java/org/codecraftlabs/tools/StringUtil.java
@@ -0,0 +1,13 @@
+package org.codecraftlabs.tools;
+
+import java.util.Optional;
+
+public class StringUtil {
+    public static boolean hasText(String value) {
+        return value != null && !value.isEmpty();
+    }
+
+    public static boolean hasText(Optional<String> value) {
+        
+    }
+}
