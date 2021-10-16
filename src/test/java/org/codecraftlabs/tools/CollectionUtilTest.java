4168444f4cad70fcc52efb28ed06a327e4678215
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2021-10-16 08:43:43 +0200

    add few more tests

--- src/test/java/org/codecraftlabs/tools/CollectionUtilTest.java
+++ src/test/java/org/codecraftlabs/tools/CollectionUtilTest.java
@@ -2,9 +2,16 @@ package org.codecraftlabs.tools;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
+import static java.util.Collections.EMPTY_LIST;
+import static java.util.Collections.EMPTY_MAP;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.emptyMap;
+import static org.codecraftlabs.tools.CollectionUtil.containsItems;
 import static org.codecraftlabs.tools.CollectionUtil.isNullOrEmpty;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -19,7 +26,39 @@ public class CollectionUtilTest {
 
     @Test
     public void isEmptyCollectionOk() {
-        var emptyCollection = new ArrayList<>();
-        assertThat(isNullOrEmpty(emptyCollection), is(true));
+        assertThat(isNullOrEmpty(emptyList()), is(true));
+    }
+
+    @Test
+    public void isNullMapOk() {
+        Map<String, String> nullMap = null;
+        assertThat(isNullOrEmpty(nullMap), is(true));
+    }
+
+    @Test
+    public void isEmptyMapOk() {
+        assertThat(isNullOrEmpty(EMPTY_MAP), is(true));
+    }
+
+    @Test
+    public void collectionContainsItemsOk() {
+        List<String> items = Collections.singletonList("test");
+        assertThat(containsItems(items), is(true));
+    }
+
+    @Test
+    public void collectionContainsItemsFalse() {
+        assertThat(containsItems(EMPTY_LIST), is(false));
+    }
+
+    @Test
+    public void mapContainsItemsOk() {
+        Map<String, String> items = Map.of("test", "test");
+        assertThat(containsItems(items), is(true));
+    }
+
+    @Test
+    public void mapContainsItemsFalse() {
+        assertThat(containsItems(EMPTY_MAP), is(false));
     }
 }
